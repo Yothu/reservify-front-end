c9b583347edd1328e4a9e2201253c423261865ec
Bondok6 <kirolousbondok6@gmail.com>
2022-06-03 00:29:32 +0200

    Create the SideNav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Hotels from './pages/Hotels';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div>
+      <header>
+        <Sidebar />
+      </header>
       <Routes>
         <Route path="/" element={<Hotels />} />
       </Routes>
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import { IconContext } from 'react-icons';
+import style from './Sidebar.module.css';
+import logo from '../../images/logo.png';
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#FBBC05' }}>
+        <Link to="#" className={style.menu}>
+          <FaIcons.FaBars onClick={showSidebar} />
+        </Link>
+
+        <nav
+          className={
+            sidebar ? `${style.navMenu} ${style.active}` : `${style.navMenu}`
+          }
+        >
+          <ul onClick={showSidebar}>
+            <li className={style.navbarToggle}>
+              <Link to="/" className={style.menu}>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" className={style.menu}>
+                <img src={logo} alt="logo" width={150} />
+              </Link>
+            </li>
+
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={style.navText}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+
+          <ul className={style.social}>
+            <li>
+              <a href="https://www.facebook.com" target="_blank">
+                <FaIcons.FaFacebookF />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com" target="_black">
+                <FaIcons.FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.pinterest.com/" target="_black">
+                <FaIcons.FaPinterestP />
+              </a>
+            </li>
+          </ul>
+
+          <p className="fw-light mt-3 text-center">
+            &copy; 2022, RESERVIFY TEAM.
+          </p>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
