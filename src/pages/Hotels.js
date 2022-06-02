e2eee188c30f65a4e5059d2b421e0269b2323b1f
Bondok6 <kirolousbondok6@gmail.com>
2022-06-02 19:52:42 +0200

    Fix toolkit issur

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,7 +1,24 @@
-const Hotels = () => (
-  <>
-    <p className="fs-2">Hotels Index</p>
-  </>
-);
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment } from '../redux/counter/counter-slice';
+
+function Hotels() {
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state.counter.value);
+
+  const increase = () => {
+    dispatch(increment(5));
+  };
+
+  return (
+    <div className="text-center">
+      Hotels :
+      {value}
+      <button type="button" onClick={increase}>
+        +
+      </button>
+    </div>
+  );
+}
 
 export default Hotels;
--- src/redux/counter/counter-slice.js
+++ src/redux/counter/counter-slice.js
@@ -4,14 +4,11 @@ const counterSlice = createSlice({
   name: 'counter',
   initialState: { value: 0 },
   reducers: {
-    // increment(state, action) {
-    //   state.value += 1;
-    // },
-    // decrement(state, action) {
-    //   state.value -= 1;
-    // },
+    increment(state, action) {
+      state.value += action.payload;
+    },
   },
 });
 
-// export const { increment, decrement } = counterSlice.actions;
+export const { increment } = counterSlice.actions;
 export default counterSlice.reducer;
