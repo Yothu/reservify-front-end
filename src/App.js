7db4bb00eb2aeddc2bceff9eac2b7e4c19e8e327
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-02 17:19:29 -0600

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Hotels from './pages/Hotels';
+import LogIn from './pages/registration/LogIn';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Hotels />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
     </div>
   );
---
+++ src/pages/registration/LogIn.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function LogIn() {
+  return (
+    <div className="d-flex flex-column align-items-center pt-5">
+      <form>
+        <h3>Log In</h3>
+        <div className="mb-3">
+          <label htmlFor="temp-email">
+            Email address
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              id="temp-email"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="temp-password">
+            Password
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              id="temp-password"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <label className="custom-control-label" htmlFor="customCheck1">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot
+          {' '}
+          <a href="/">password?</a>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default LogIn;
