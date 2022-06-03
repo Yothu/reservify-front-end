09c207e5dde8a9d15543a5d25e4b696ab44e761e
Bondok6 <kirolousbondok6@gmail.com>
2022-06-03 11:31:57 +0200

    Install react-hook-form and Create SingupForm

--- src/components/Signup/SignupForm.jsx
+++ src/components/Signup/SignupForm.jsx
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Button from './../Ui/Button';
+import { useForm } from 'react-hook-form';
+import ErrorMsg from './../Ui/ErrorMsg';
 
 const SignupForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const password = useRef({});
+  password.current = watch('password');
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="d-flex flex-column align-items-center gap-2">
-      <input type="text" placeholder="Username" className="form-control w-75" />
-      <input type="text" placeholder="Email" className="form-control w-75" />
-      <input type="text" placeholder="Password" className="form-control w-75" />
+    <form
+      className="d-flex flex-column align-items-center gap-2"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <input
         type="text"
+        placeholder="Username"
+        className="form-control w-75"
+        {...register('name', { required: 'Username is required' })}
+      />
+      {errors.name && <ErrorMsg message={errors.name.message} cName="w-75" />}
+      <input
+        type="text"
+        placeholder="Email"
+        className="form-control w-75"
+        {...register('email', { required: 'Email is Required' })}
+      />
+      {errors.email && <ErrorMsg message={errors.email.message} cName="w-75" />}
+      <input
+        type="password"
+        placeholder="Password"
+        className="form-control w-75"
+        {...register('password', {
+          required: 'Password is Required',
+          minLength: { value: 6, message: 'Password is Too Short' },
+        })}
+      />
+      {errors.password && (
+        <ErrorMsg message={errors.password.message} cName="w-75" />
+      )}
+      <input
+        type="password"
         placeholder="Confim Password"
         className="form-control w-75"
+        {...register('confirm_password', {
+          validate: (value) =>
+            value === password.current || 'The passwords do not match',
+        })}
       />
+      {errors.confirm_password && (
+        <ErrorMsg message={errors.confirm_password.message} cName="w-75" />
+      )}
       <Button type="submit" text="Signup" cName="mt-3" />
     </form>
   );
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import SignupForm from './../../components/Signup/SignupForm';
 import registerImg from '../../images/undraw-svg/register.svg';
-import style from './Signup.module.css';
 
 function Signup() {
   return (
-    <section className={`${style.signup} mt-5`}>
+    <section className="mt-5">
       <div className="text-center mb-3">
         <img src={registerImg} alt="register" className="mw-100" width={500} />
       </div>
