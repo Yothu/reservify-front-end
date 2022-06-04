8b7ba62a02b28e9c4fb509f7492fc9513aa80660
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-03 20:45:45 -0600

    Test email validations

--- src/tests/LogIn.test.js
+++ src/tests/LogIn.test.js
@@ -4,24 +4,46 @@ import {
   render,
   prettyDOM,
   fireEvent,
-  act
+  act,
 } from '@testing-library/react';
 import LogIn from '../pages/Registration/LogIn';
 
 describe('<Log In />', () => {
   let component;
+  let emailField;
+  let passwordField;
   let submitBtn;
+
+  const mockEmail = 'abc';
 
   beforeEach(() => {
     component = render(<LogIn />);
+    emailField = component.container.querySelector("input[name='email']");
+    passwordField = component.container.querySelector("input[name='password']");
     submitBtn = component.getByRole('button', { name: /submit/i });
   });
-  
-  
+
   test('renders form corrently', () => {
     component.getByText('Log In');
     component.getByLabelText('Email', { exact: false });
     component.getByLabelText('Password');
     component.getByRole('button', { name: /submit/i });
+  });
+
+  test('if email field is empty on submit, renders "email required"', async () => {
+    await act(async () => {
+      fireEvent.submit(submitBtn);
+    });
+
+    component.getByText('Email is required', { exact: false });
+  });
+
+  test('should display correct error message for email miss match', async () => {
+    await act(async () => {
+      fireEvent.input(emailField, { target: { value: mockEmail } });
+      fireEvent.submit(submitBtn);
+    });
+
+    component.getByText('Invalid', { exact: false });
   });
 });
