7d45b75ea2b35275e99e1f81209e888a068f35b7
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-06 15:52:54 -0600

    Add route for hotel's detail page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import Signup from './pages/registration/signup/Signup';
 import Sidebar from './components/Sidebar/Sidebar';
 import LogIn from './pages/registration/LogIn';
 import Reserve from './pages/reserve/Reserve';
+
+import Hotels from './pages/Hotels';
+import HotelDetails from './pages/HotelDetails';
+
 import './App.css';
 
 function App() {
+  const hotels = [
+    {
+      id: '1', name: 'Hilton',
+    },
+    {
+      id: '2', name: 'Sheraton',
+    },
+  ];
   return (
     <>
       <header>
@@ -18,6 +30,10 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/reserve" element={<Reserve />} />
+
+          <Route path="/hotels" element={<Hotels hotels={hotels} />} />
+          <Route path="/hotels/:id" element={<HotelDetails hotels={hotels} />} />
+
         </Routes>
       </main>
     </>
