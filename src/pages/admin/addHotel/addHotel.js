a9a3939248c8b858ba1771714ab216908a55a96e
David Vergaray <david.vergaray.a@gmail.com>
2022-06-06 16:51:22 -0400

    Create AddHotel page

---
+++ src/pages/admin/addHotel/addHotel.js
@@ -0,0 +1,13 @@
+import AddHotelForm from '../../../components/AddHotel/AddHotelForm';
+
+const AddHotel = () => (
+  <section>
+    <div>
+      <h1>Add Hotel</h1>
+    </div>
+
+    <AddHotelForm />
+  </section>
+);
+
+export default AddHotel;
