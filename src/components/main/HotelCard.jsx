1cf313020195ca01cc49fd92792bf1f7a3f9cf3d
Bondok6 <kirolousbondok6@gmail.com>
2022-06-07 07:19:07 +0200

    Add Social Icons

--- src/components/main/HotelCard.jsx
+++ src/components/main/HotelCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { IconContext } from 'react-icons';
+import * as FaIcons from 'react-icons/fa';
 import style from './HotelCard.module.css';
 
 const HotelCard = ({ image, name, description }) => (
@@ -9,6 +11,25 @@ const HotelCard = ({ image, name, description }) => (
       <h4>{name}</h4>
       <h5>{description}</h5>
     </div>
+    <IconContext.Provider value={{ color: '#d3d3d3' }}>
+      <ul>
+        <li>
+          <a href="https://www.facebook.com" rel="noreferrer" target="_blank">
+            <FaIcons.FaFacebookF />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com" rel="noreferrer" target="_blank">
+            <FaIcons.FaTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.pinterest.com/" rel="noreferrer" target="_blank">
+            <FaIcons.FaPinterestP />
+          </a>
+        </li>
+      </ul>
+    </IconContext.Provider>
   </div>
 );
 
