534077a16a06f996966277bfd86a32228b06fad0
David Vergaray <david.vergaray.a@gmail.com>
2022-06-07 17:06:08 -0400

    Add hotel reducer

---
+++ src/redux/hotels/hotel-slice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const hotelSlice = createSlice({
+  name: 'hotel',
+  initialState: {
+    hotels: [
+      {
+        id: 1,
+        name: 'hotel1',
+      },
+      {
+        id: 2,
+        name: 'hotel2',
+      },
+      {
+        id: 3,
+        name: 'hotel3',
+      },
+      {
+        id: 4,
+        name: 'hotel4',
+      },
+      {
+        id: 5,
+        name: 'hotel5',
+      },
+    ],
+  },
+  reducers: {
+    deleteHotel(state, action) {
+      // fetch data
+      state.hotels.splice(state.hotels.findIndex((hotel) => hotel.id === action.payload.id), 1);
+    },
+  },
+});
+
+export const { deleteHotel } = hotelSlice.actions;
+export default hotelSlice.reducer;
