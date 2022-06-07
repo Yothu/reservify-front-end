3dba733c68e26f0bdefa655f2419b84922d50ce9
David Vergaray <david.vergaray.a@gmail.com>
2022-06-07 17:31:32 -0400

    fix linters

--- src/components/DeleteHotel/DeleteHotelList.js
+++ src/components/DeleteHotel/DeleteHotelList.js
@@ -7,7 +7,7 @@ const DeleteHotelList = () => {
   return (
     <table className={style['delete-table']}>
       <thead>
-     <tr className="fs-5">
+        <tr className="fs-5">
           <th>Hotel Name</th>
           <th>Action</th>
         </tr>
