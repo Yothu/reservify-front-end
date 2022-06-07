b298c139c93330fe76ecc49727f04d302137ee06
David Vergaray <david.vergaray.a@gmail.com>
2022-06-07 17:07:08 -0400

    Create DeleteHotelList component

--- src/components/DeleteHotel/DeleteHotelList.js
+++ src/components/DeleteHotel/DeleteHotelList.js
@@ -1,5 +1,26 @@
-const DeleteHotelList = () => (
-  <p>HOTEL LIST!</p>
-);
+import { useSelector } from 'react-redux';
+import DeleteHotelListRow from './DeleteHotelListRow';
+import style from './DeleteHotelList.module.css';
+
+const DeleteHotelList = () => {
+  const hotels = useSelector((state) => state.hotel.hotels);
+  return (
+    <table className={style['delete-table']}>
+      <thead>
+        <tr>
+          <th>Hotel Name</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          hotels.map((hotel) => (
+            <DeleteHotelListRow key={hotel.id} id={hotel.id} name={hotel.name} />
+          ))
+        }
+      </tbody>
+    </table>
+  );
+};
 
 export default DeleteHotelList;
