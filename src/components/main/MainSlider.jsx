5891280c114f94d0c5c88a271a4014bc4404f321
Bondok6 <kirolousbondok6@gmail.com>
2022-06-07 06:38:24 +0200

    Add Arrows using react-icon

--- src/components/main/MainSlider.jsx
+++ src/components/main/MainSlider.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import { React, useRef } from 'react';
+import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
 import HotelCard from './HotelCard';
 import HotelsData from './HotelsData';
 import style from './MainSlider.module.css';
 
-const MainSlider = () => (
-  <>
-    <div className={style['slider-container']}>
-      <div className={style.slider}>
-        {HotelsData.map((hotel) => (
-          <HotelCard
-            key={hotel.id}
-            image={hotel.image}
-            name={hotel.name}
-            description={hotel.description}
-          />
-        ))}
+const MainSlider = () => {
+  const slider = useRef();
+  const rightArrow = useRef();
+  const leftArrow = useRef();
+
+  return (
+    <>
+      <div className={style['slider-container']}>
+        <div className={style.slider} ref={slider}>
+          {HotelsData.map((hotel) => (
+            <HotelCard
+              key={hotel.id}
+              image={hotel.image}
+              name={hotel.name}
+              description={hotel.description}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </>
-);
+      <div className={style.left_arrow} ref={leftArrow}>
+        <BiLeftArrow />
+      </div>
+      <div className={style.right_arrow} ref={rightArrow}>
+        <BiRightArrow />
+      </div>
+    </>
+  );
+};
 
 export default MainSlider;
