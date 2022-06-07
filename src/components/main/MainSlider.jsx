ae5983f5dfcb3094eb34a2cb3b679e0e9df9afef
Bondok6 <kirolousbondok6@gmail.com>
2022-06-07 06:53:39 +0200

    Add Slider Functionality

--- src/components/main/MainSlider.jsx
+++ src/components/main/MainSlider.jsx
@@ -1,4 +1,4 @@
-import { React, useRef } from 'react';
+import { React, useRef, useState } from 'react';
 import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
 import HotelCard from './HotelCard';
 import HotelsData from './HotelsData';
@@ -8,6 +8,23 @@ const MainSlider = () => {
   const slider = useRef();
   const rightArrow = useRef();
   const leftArrow = useRef();
+  const [counter, setCounter] = useState(0);
+  const size = 340;
+
+  const nextHandler = () => {
+    if (counter === HotelsData.length - 1) return;
+    slider.current.style.transition = 'transform 0.8s ease-in-out';
+    const c = counter + 1;
+    slider.current.style.transform = `translateX(${-size * c}px)`;
+    setCounter(c);
+  };
+
+  const prevHandler = () => {
+    if (counter === 0) return;
+    const c = counter - 1;
+    slider.current.style.transform = `translateX(${-size * c}px)`;
+    setCounter(c);
+  };
 
   return (
     <>
@@ -23,10 +40,20 @@ const MainSlider = () => {
           ))}
         </div>
       </div>
-      <div className={style.left_arrow} ref={leftArrow}>
+      <div
+        className={style.left_arrow}
+        ref={leftArrow}
+        onClick={prevHandler}
+        aria-hidden="true"
+      >
         <BiLeftArrow />
       </div>
-      <div className={style.right_arrow} ref={rightArrow}>
+      <div
+        className={style.right_arrow}
+        ref={rightArrow}
+        onClick={nextHandler}
+        aria-hidden="true"
+      >
         <BiRightArrow />
       </div>
     </>
