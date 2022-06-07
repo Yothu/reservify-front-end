be42b63014db054abdea5d1441478d0bb4fc00c5
David Vergaray <david.vergaray.a@gmail.com>
2022-06-07 17:06:45 -0400

    Add flex to delete hotel table

--- src/pages/admin/deleteHotel/DeleteHotel.js
+++ src/pages/admin/deleteHotel/DeleteHotel.js
@@ -1,7 +1,7 @@
 import DeleteHotelList from '../../../components/DeleteHotel/DeleteHotelList';
 
 const DeleteHotel = () => (
-  <section>
+  <section className="h-100 d-flex flex-column align-items-center">
     <div className="text-center my-5">
       <h1>Delete Hotel</h1>
     </div>
