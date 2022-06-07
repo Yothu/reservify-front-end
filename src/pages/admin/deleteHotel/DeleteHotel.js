0dcf2c00fd7d6ec91c59b1eec5f45f8b8c290e30
David Vergaray <david.vergaray.a@gmail.com>
2022-06-07 12:02:50 -0400

    Add Delete hotel page

---
+++ src/pages/admin/deleteHotel/DeleteHotel.js
@@ -0,0 +1,13 @@
+import DeleteHotelList from '../../../components/DeleteHotel/DeleteHotelList';
+
+const DeleteHotel = () => (
+  <section>
+    <div className="text-center my-5">
+      <h1>Delete Hotel</h1>
+    </div>
+
+    <DeleteHotelList />
+  </section>
+);
+
+export default DeleteHotel;
