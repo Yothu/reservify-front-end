24376051a1cbbb5191ad18aa1ed144748d0c9961
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-08 13:28:19 -0600

    Install luxon library for dates

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,4 +1,9 @@
+import { DateTime } from 'luxon';
+
 const MyReservations = () => {
+  const currentTime = DateTime.now().toLocaleString();
+  console.log(currentTime);
+  
   return (
     <div className="text-center">
       <h1>My Reservations</h1>
