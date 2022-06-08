80446a3d1cecb9895294e706ef372d0b42ae8cd4
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-06-08 15:24:18 -0600

    Add cancel button and sidebar link to reservations

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -28,15 +28,16 @@ const MyReservations = () => {
     <>
       <h1 className="text-center mt-5">My Reservations</h1>
       <div className="d-flex flex-column">
-        <table className="table">
+        <table className="table table-bordered table-hover">
           <caption>All your reservations</caption>
-          <thead className="">
+          <thead className="table-dark">
             <tr>
               <th scope="col">Hotel Name</th>
               <th scope="col">Price</th>
               <th scope="col" className="d-none d-sm-block">Stars</th>
               <th scope="col">Check-in</th>
               <th scope="col" className="d-none d-sm-block">Check-out</th>
+              <th scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -47,9 +48,10 @@ const MyReservations = () => {
                   $
                   {reservation.price}
                 </td>
-                <td className="d-none d-sm-block">{reservation.stars}</td>
+                <td className="d-none d-sm-table-cell">{reservation.stars}</td>
                 <td>{reservation.start_date.toLocaleString()}</td>
-                <td className="d-none d-sm-block">{reservation.end_date.toLocaleString()}</td>
+                <td className="d-none d-sm-table-cell">{reservation.end_date.toLocaleString()}</td>
+                <td><button type="button" className="btn btn-outline-danger">Cancel</button></td>
               </tr>
             ))}
           </tbody>
