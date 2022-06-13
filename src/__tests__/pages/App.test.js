067c95437c49f4547a0e2336ce7b19957942485a
Bondok6 <kirolousbondok6@gmail.com>
2022-06-14 01:03:24 +0200

    Add __tests__

---
+++ src/__tests__/pages/App.test.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../redux/store';
+import App from '../../App';
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/__tests__/pages/LogIn.test.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../redux/store';
+import LogIn from '../../pages/registration/LogIn';
+
+describe('Login Page', () => {
+  it('renders correctly', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <LogIn />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
